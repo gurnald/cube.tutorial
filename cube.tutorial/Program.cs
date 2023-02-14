4e39429f148d95c79e679c5f20ad88568610bbd3
Jacob Roberts <jacobroberts9797@hotmail.com>
2023-02-14 10:25:57 -0500

    Add project files.

---
+++ cube.tutorial/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+/*
+var nbr = 1;
+    while (nbr <= 15)
+    {
+    if (nbr % 2 == 0) {
+        nbr++;
+        continue;
+
+    }
+    Console.WriteLine($"{nbr}^3 is {nbr * nbr * nbr}");
+    nbr++;
+
+    Console.WriteLine("Here is your number, Gurn!");
+}
+*/
+/*
+var nbr = 1;
+while (nbr <= 25) 
+{
+    if(nbr % 2 == 0) { 
+        nbr++;
+        continue;
+    }
+    Console.WriteLine($"{nbr}^3 is {nbr * nbr * nbr}");
+   
+    nbr++;
+}
+Console.WriteLine("Done.");
+*/
